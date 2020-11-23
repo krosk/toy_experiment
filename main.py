ab9d7e5c76c769d116d141da26c851c09daae383
Alexis He <ahe@slb.com>
2020-11-23 21:39:50 +0100

    Add: query img by range

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ conn = sqlite3.connect('challenge.db')
 
 # -------------------------------------------- #
 #   Database
+#   https://docs.python.org/2/library/sqlite3.html
 # -------------------------------------------- #
 
 def drop_table_for_img(name):
@@ -28,11 +29,21 @@ def insert_img_into_table(name, column_count, img_rmo_npy):
     img_rmo_tuple_list = [tuple(row) for row in img_rmo_npy]
     placeholder = '?' + ',?' * column_count
     c.executemany(f'INSERT INTO {name} VALUES ({placeholder})', img_rmo_tuple_list)
+    conn.commit()
+    
 
-def initialize_table_for_img(name, column_count, img_rmo_npy):
+def initialize_table_for_img(name, img_rmo_npy):
+    column_count = img_rmo_npy.shape[1] - 1
     drop_table_for_img(name)
     create_table_for_img(name, column_count)
     insert_img_into_table(name, column_count, img_rmo_npy)
+
+def query_img_in_range(name, depth_min, depth_max):
+    c = conn.cursor()
+    range_tuple = (depth_min, depth_max)
+    select_query = f'SELECT * from {name} WHERE depth BETWEEN ? AND ?';
+    for row in c.execute(select_query, range_tuple):
+        print(c.fetchone())
 
 # -------------------------------------------- #
 #   Application
@@ -83,7 +94,9 @@ def run(addr, port, server_class=HTTPServer, handler_class=HandlerChallenge):
     httpd = server_class(server_address, handler_class)
 
     full_array_rmo_npy = convert_csv_to_data_npy('img.csv')
-    initialize_table_for_img('img', 200, full_array_rmo_npy)
+    initialize_table_for_img('img', full_array_rmo_npy)
+    
+    query_img_in_range('img', 9100, 9200)
 
     print(f"Starting httpd server on {addr}:{port}")
     httpd.serve_forever()
